4ad4ccc1ea0fc7bb5164ff001b257067b2d3e884
RICARDO ALVAREZ <ricardo.alvarez@normaltandil.edu.ar>
2026-08-25 09:49:11 -0300

    Use flattened V2 application entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import AppV2 from './v2/AppV2.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppV2 />
+    <App />
   </StrictMode>,
 )
