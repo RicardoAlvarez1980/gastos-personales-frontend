49e7706417545cde937645f29a390b7107c2703c
RICARDO ALVAREZ <ricardo.alvarez@normaltandil.edu.ar>
2026-08-25 07:30:28 -0300

    feat(v2): make V2 the entry point on V2 branch

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-
+import AppV2 from './v2/AppV2.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppV2 />
   </StrictMode>,
 )
